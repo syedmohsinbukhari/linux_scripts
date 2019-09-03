8ef24e48970fe38b0eb1851b9d352e926d12b6ca
mohsin-hazen-ai <mohsin@hazen.ai>
2019-09-03 15:40:44 +0500

    added a script which detects if it is run through bash or source

---
+++ bash_or_source.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+script_name=$( basename ${0#-} ) #- needed if sourced no path
+this_script=$( basename ${BASH_SOURCE} )
+if [[ ${script_name} = ${this_script} ]] ; then
+    echo "running me directly"
+else
+    echo "sourced from ${script_name}"
+fi 
